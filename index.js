0348c7019527ac322fb6bbde4357988f05e3c352
Rakib <hellorakibhossen@gmail.com>
2025-02-02 22:56:01 +0600

    create api for blog

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 9000;
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.get("/", async (req, res) => {
   res.send("Skillexa server is comming");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hsnri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,6 +23,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const blogCollections = client.db("skillexaDb").collection("blogs");
+
+    // blog get api
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollections.find().toArray();
+      res.send(result);
+    });
+    // specific blog get api
+
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollections.findOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
